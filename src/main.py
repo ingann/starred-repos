34376ccd0db931ed75acd5da1c2eb570fdec8fc9
ingann <inka.niinisalo@helsinki.fi>
2024-02-25 02:05:47 +0200

    added error handling, updated code to return correct result

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import httpx
 
 app = FastAPI()
 github_client_id = 'bc59d750d6799f0c983e'
+#add your own secret key to github_client_secret
 github_client_secret = ''
 
 @app.get('/')
@@ -13,6 +14,10 @@ async def github_login():
 
 @app.get('/callback')
 async def callback(code: str):
+
+    if not code:
+        raise HTTPException(status_code=400, detail="Invalid code parameter")
+
     params = {
         'client_id': github_client_id,
         'client_secret': github_client_secret,
@@ -25,12 +30,17 @@ async def callback(code: str):
     try:
         async with httpx.AsyncClient() as client:
             response = await client.post(url='https://github.com/login/oauth/access_token', params=params, headers=headers)
+        response.raise_for_status()
         response_json = response.json()
         access_token = response_json['access_token']
         async with httpx.AsyncClient() as client:
             headers.update({'Authorization': f'Bearer {access_token}'})
-            response = await client.get('https://api.github.com/user', headers=headers)
-        return response.json()
+            response = await client.get('https://api.github.com/user/starred', headers=headers)
+        response.raise_for_status()
+        response_json = response.json()
+        return get_starred_repos(response_json)
+
+
     except HTTPStatusError as http_error:
         status_code = http_error.response.status_code
         if status_code == 401:
@@ -41,3 +51,27 @@ async def callback(code: str):
             raise HTTPException(status_code=500, detail="Internal Server Error")
     except Exception:
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
+def create_dic_object(repo):
+    
+    topics = ', '.join(repo['topics']) if 'topics' in repo else ''
+    obj = {
+        'name': repo['full_name'],
+        'desc': repo['description'],
+        'URL': repo['url'],
+        'license': repo['license']['name'] if repo['license'] else '',
+        'topics': topics
+    }
+    return obj
+
+def get_starred_repos(response):
+    repos = []
+    amount = 0
+    for repo in response:
+        if not repo['private']:
+            repos.append(create_dic_object(repo))
+            amount += 1
+    result = {'number_of_public_starred_repos': amount, 'starred_repos': repos}
+    return result
+    
+
